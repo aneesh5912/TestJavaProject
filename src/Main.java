c2e2ec6db12c4375b948251f88c33254f830df5d
Aneesh <aneeshshrivastava0@gmail.com>
2023-07-07 23:01:17 +0530

    my first project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Human h = new Human();
+        h.setAge(25);
+        h.setName("Aneesh");
+        System.out.println(h.getName());
+
+    }
+}
